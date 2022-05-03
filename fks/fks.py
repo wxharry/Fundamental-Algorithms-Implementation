1915ed650b999a622d3537d2e2de43f1a7f3b959
Harry <xiaohanwu12@gmail.com>
2022-05-03 12:23:56 -0400

    Add isGood

--- fks/fks.py
+++ fks/fks.py
@@ -1,7 +1,12 @@
+from email.policy import default
 from math import ceil
+
+from attr import has
+from numpy import empty
 
 K_const = """A, ABOUT, AN, AND, ARE, AS, AT, BE, BOY, BUT, BY, FOR, FROM, HAD, HAVE, HE, HER, HIS, HIM, I, IN, IS, IT, NOT, OF, ON, OR, SHE, THAT, THE, THEY, THIS, TO, WAS, WHAT, WHERE, WHICH, WHY, WITH, YOU"""
 K = K_const.split(", ")
+n = len(K)
 p = 2
 
 def adic26(word:str)->int:
@@ -24,10 +29,22 @@ def findPrime(n:int)->int:
             found = True
     return n;
 
-# TODO: implement function isGood()
-def isGood(k:int)->bool:
-    pass
+def hash(x, k):
+    if isinstance(x, str):
+        x = adic26(x)
+    return ((x*k % p) % n)
     
+def isGood(k:int)->bool:
+    bin_dict = dict()
+    for w in K:
+        bin_dict[hash(w, k)] = bin_dict.get(hash(w, k), 0) + 1
+    sum = 0
+    for key, val in bin_dict.items():
+        sum += val * (val - 1) / 2
+        if sum >= n:
+            return False
+    return sum < n 
+
 
 maxWord = K[0]
 for word in K:
@@ -35,4 +52,27 @@ for word in K:
         maxWord = word
 print(maxWord, adic26(maxWord))     # WHICH 10657226
 p = findPrime(adic26(maxWord))
-print(p)   # p = 10657247
+print("Prime is", p)   # p = 10657247
+
+# pick a good k and display the bins
+for k in range(p):
+    if isGood(k):
+        print(str(k) + " is a good k")
+        bin_dict = dict()
+        # Collection bins
+        for w in K:
+            bin_dict[hash(w, k)] = bin_dict.get(hash(w, k), []) + [w]
+        
+        b_array = []
+        for i in range(n):
+            bi:list = bin_dict.get(i, [])
+            b_array.append(len(bi))
+            # List all the non-empty Bi's
+            if len(bi):
+                print("B_{0} = {1}".format(i, bi))
+        # show the b[0-39] array
+        print(b_array)
+        break
+
+
+
